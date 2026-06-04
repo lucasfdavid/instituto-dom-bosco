ea72b0e4e4af55d5c7faaf3d1aca57435ac58720
lucasfdavid <lucas_fernandes50@hotmail.com>
2026-06-04 01:52:39 -0300

    Fix done style in miniStyle object

--- src/app/aluno/historico/page.tsx
+++ src/app/aluno/historico/page.tsx
@@ -11,4 +11,5 @@ const miniStyle: Record<string, string> = {
   D1: 'bg-d1-bg text-d1',
   D7: 'bg-d7-bg text-d7',
   D30: 'bg-d30-bg text-d30',
-  done: 'bg-done-bg
+  done: 'bg-done-bg text-done',
+}
