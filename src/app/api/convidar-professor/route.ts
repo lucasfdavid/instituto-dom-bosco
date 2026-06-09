15afbb729929ec83a77424cf718ba9f0510b58a6
lucasfdavid <lucas_fernandes50@hotmail.com>
2026-06-08 23:38:35 -0300

    Change redirect URL for professor invitation

--- src/app/api/convidar-professor/route.ts
+++ src/app/api/convidar-professor/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
 
     const { error } = await supabase.auth.admin.inviteUserByEmail(email, {
       data: { role: 'professor' },
-      redirectTo: 'https://instituto-dom-bosco.vercel.app/auth/login'
+      redirectTo: 'https://instituto-dom-bosco.vercel.app/auth/confirmar'
     })
 
     if (error) throw error
